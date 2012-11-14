a862ba72a4a46699a5f44c1e3af052b7bcd1d788
yurilavinas <yurilavinas@cjr.org.br>
2012-11-14 11:44:06 +0000

    x

---
+++ spec/application/controllers/administrador.php
@@ -0,0 +1,103 @@
+<?php
+
+class Administrador extends CI_Controller {
+    
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->library('grocery_CRUD');
+    }
+    
+    function _chamaTemplate($output = null)
+    {
+        $this->load->view('adminTemplateView.php',$output);    
+    }
+        
+    function index()
+    {
+        if (    $this->session->userdata('logado') == true  )
+        {
+            $this->load->model('adminModel');
+            $output = $this->adminModel->crudAdmin($this->verificaAdmin());
+            $this->_chamaTemplate($output);
+            
+        }
+        else
+        {
+            redirect('login');
+        }
+
+    }
+    
+    function dica()
+    {
+        $this->load->model('adminModel');
+        $output = $this->adminModel->crudDica();
+        $this->_chamaTemplate($output);
+    }
+    
+    function noticia()
+    {
+        $this->load->model('adminModel');
+        $output = $this->adminModel->crudNoticia();
+        $this->_chamaTemplate($output);
+    }
+    
+    function blog()
+    {
+        $this->load->model('adminModel');
+        $output = $this->adminModel->crudBlog();
+        $this->_chamaTemplate($output);
+    }
+    
+    function prefeitura()
+    {
+        $this->load->model('adminModel');
+        $output = $this->adminModel->crudPrefeitura();
+        if( $this->session->userdata('erro') == 'insert' ) $output['erro'] = 'insert';
+        $this->_chamaTemplate($output);
+    }
+    
+    function secretario()
+    {       
+        $this->load->model('adminModel');
+        $output = $this->adminModel->crudSecretario();
+        
+        $this->_chamaTemplate($output); 
+
+    }
+    
+    function verificaAdmin()
+    {
+        $this->load->model('usuarioModel');        
+        $tipoDeUsuario = $this->usuarioModel->verificaAdmin();   
+        if(is_object($tipoDeUsuario))
+        {
+            //usuário é admin
+            if( $tipoDeUsuario->administrador_id == 0)
+            {
+                return 0;
+            }
+            else
+            {//admin normal
+                return 1;
+            }
+                    
+        }
+        else
+        {//usuário normal
+           return 2;
+        }
+        
+    }
+    
+    function checaQtdSecretarios($id)
+    {
+        
+        $this->load->model('adminModel');
+        $output = $this->adminModel->checaQtd($id);
+        echo $output;
+                
+    }
+    
+}
