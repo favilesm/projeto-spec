107ebab85323aa7d5ad200cd10f07dd886e7240e
matheuspimenta <matheuspimenta@cjr.org.br>
2012-12-12 15:28:58 +0000

    Comecei a implementar as coisas de prefeitura

--- spec/application/views/admin/top.php
+++ spec/application/views/admin/top.php
@@ -1,17 +1,16 @@
 
-    <div>
-        <a href='<?php echo site_url('administrador')?>'>Administradores</a> | 
-        <a href='<?php echo site_url('administrador/dica')?>'>Dicas</a> |
-        <a href='<?php echo site_url('administrador/noticia')?>'>Notícias</a> |
-        <a href='<?php echo site_url('administrador/blog')?>'>Blog</a> |
-        <a href='<?php echo site_url('administrador/prefeitura')?>'>Prefeitura</a> |
-        <a href='<?php echo site_url('administrador/prefeito')?>'>Prefeito</a> |
-        <a href='<?php echo site_url('administrador/secretario')?>'>Secretario</a> |
-        <a href='<?php echo site_url('administrador/programa')?>'>Programa do governo</a> |
-        <a href='<?php echo site_url('administrador/mensagem')?>'>Mensagem a um usuário</a> 
+<div>
+    <a href='<?php echo site_url('administrador')?>'>Administradores</a> | 
+    <a href='<?php echo site_url('administrador/dica')?>'>Dicas</a> |
+    <a href='<?php echo site_url('administrador/noticia')?>'>Notícias</a> |
+    <a href='<?php echo site_url('administrador/blog')?>'>Blog</a> |
+    <a href='<?php echo site_url('administrador/prefeitura')?>'>Prefeitura</a> |
+    <a href='<?php echo site_url('administrador/prefeito')?>'>Prefeito</a> |
+    <a href='<?php echo site_url('administrador/secretario')?>'>Secretario</a> |
+    <a href='<?php echo site_url('administrador/programa')?>'>Programa do governo</a> |
+    <a href='<?php echo site_url('administrador/mensagem')?>'>Mensagem a um usuário</a> 
+</div>
 
-
-    </div>
-    <div>
-        <a href='<?php echo site_url('login/logout')?>'>Sair</a>
-    </div>
+<div>
+    <a href='<?php echo site_url('login/logout')?>'>Sair</a>
+</div>
