0d8c538bcad3e1e11b47ed11b3dae09f2678ccea
matheuspimenta@cjr.org.br <matheuspimenta@cjr.org.br@9cf948e7-e6ac-023c-6bea-9e4e62a6b0e4>
2012-12-12 16:18:47 +0000

    Fiz a query de selecao das noticias

--- trunk/spec/application/controllers/prefeitura.php
+++ trunk/spec/application/controllers/prefeitura.php
@@ -1,7 +1,13 @@
 <?php
 
 class Prefeitura extends CI_Controller {
-
+    
+    function __construct()
+    {
+        parent::__construct();
+        $this->load->model('prefeituramodel');
+    }
+    
     function index()
     {
         $this->load->view('_inc/header_open');
@@ -13,10 +19,13 @@ class Prefeitura extends CI_Controller {
     
     function noticia()
     {
+        $noticias = $this->prefeituramodel->getNoticias();
+        
         $this->load->view('_inc/header_open');
         $this->load->view('_inc/header');
         $this->load->view('_inc/header_close');
         $this->load->view('prefeitura/top');
+        $this->load->view('prefeitura/noticias', $noticias);
         $this->load->view('_inc/footer');
     }
 }
---
+++ trunk/spec/application/views/prefeitura/noticias.php
@@ -0,0 +1,7 @@
+
+<div id="aeae">
+<?php foreach ($noticias as $noticia) : ?>
+	<?php echo $noticia->titulo_noticia; ?>
+	<br />
+<?php endforeach; ?>
+</div>
