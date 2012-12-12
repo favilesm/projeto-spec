4d1d3c9a16a7cb255c77d8f0236289f71f62b3db
matheuspimenta <matheuspimenta@cjr.org.br>
2012-12-12 16:18:47 +0000

    Fiz a query de selecao das noticias

--- spec/application/models/prefeituramodel.php
+++ spec/application/models/prefeituramodel.php
@@ -2,4 +2,13 @@
 
 class PrefeituraModel extends CI_Model {
 	
+	function getNoticias()
+	{
+		$dados['noticias'] = $this->db->query(
+			'SELECT * FROM noticia INNER JOIN prefeitura_x_noticia ON '.
+			'noticia.noticia_id=prefeitura_x_noticia.noticia_noticia_id WHERE '.
+			'prefeitura_x_noticia.prefeitura_prefeitura_id='.$this->session->userdata('id')
+		)->result();
+		return $dados;
+	}
 }
